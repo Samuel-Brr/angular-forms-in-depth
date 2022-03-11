db984ab3e661b85aa3f6a7e4ece9a66731602251
Samuel-Brr <samuel.barre972@gmail.com>
2022-03-11 16:53:17 +0100

    Comparing Reactive and Template Driven Forms

--- src/app/login-reactive/login-reactive.component.ts
+++ src/app/login-reactive/login-reactive.component.ts
@@ -35,4 +35,13 @@ export class LoginReactiveComponent implements OnInit {
 
   }
 
+
+  get email(){
+    return this.form.controls['email']
+  } 
+
+  get password(){
+    return this.form.controls['password']
+  }
+
 }
