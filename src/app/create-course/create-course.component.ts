21f63a5e259cae7096b8a566e48158c0b7f400f6
Samuel-Brr <samuel.barre972@gmail.com>
2022-03-12 16:30:12 +0100

    Multi-Step Forms - Displaying an Error Message per Step

--- src/app/create-course/create-course.component.ts
+++ src/app/create-course/create-course.component.ts
@@ -7,7 +7,12 @@ import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
 @Component({
   selector: 'create-course',
   templateUrl: './create-course.component.html',
-  styleUrls: ['./create-course.component.scss']
+  styleUrls: ['./create-course.component.scss'],
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS, useValue: {showError:true} // Code pour activer la propriété errorMessage pour chaque step du stepper
+    }
+  ]
 })
 export class CreateCourseComponent implements OnInit {
 
